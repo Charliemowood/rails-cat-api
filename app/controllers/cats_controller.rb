6c9f56cdabba60fa63a8ab7797ca846444349658
Charlie Wood <charliemowood@gmail.com>
2017-07-17 16:05:46 +0100

    Add restful routes

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,34 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :json => @cat
   end
+
+  def create
+    @cat = Cat.create(cat_params)
+    render :json => @cat
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update_attributes(cat_params)
+      render :json => @cat
+    else
+      render :json => {status: :update_failed}
+    end
+  end
+
+  def destroy
+    cat = Cat.find(params[:id])
+    if cat.destroy!
+      render :json => {status: :success}
+    else
+      render :json => {status: :delete_failed}
+    end
+  end
+
+
+  private
+
+  def cat_params
+    params.require(:cat).permit([:name])
+  end
 end
