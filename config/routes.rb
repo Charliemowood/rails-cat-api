9b413e308bfb9e3fca67c53f4216edf28bf4c4a3
Charlie Wood <charliemowood@gmail.com>
2017-07-17 22:36:05 +0100

    Minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # get 'cats' => 'cats#index'
-  resources :cats
-
-  # You can have the root of your site routed with "root"
+  scope path: "api" do
+    resources :cats  
+  end  # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
